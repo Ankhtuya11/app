55be2d5825cc2411621f20827896ac2a721431e5
Kalista25 <112840908+jargalaaaa12@users.noreply.github.com>
2023-05-05 00:49:59 +0800

    change

--- Blindcare/src/screens/HomeScreen.js
+++ Blindcare/src/screens/HomeScreen.js
@@ -7,47 +7,241 @@ import {
   Image,
   StyleSheet,
 } from "react-native";
-
+import {  Ionicons,
+  Entypo,
+  MaterialCommunityIcons,
+  Octicons,
+  AntDesign,} from "@expo/vector-icons"
+import { EvilIcons, FontAwesome } from "expo-vector-icons";
+ 
 export default function HomeScreen({ navigation }) {
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <View style={styles.vm1}>
+    <ScrollView>
+          <View style={styles.vm1}>
           <Image
             style={styles.pro1}
             source={require("../../src/images/natural.jpg")}
           />
-          <Text style={styles.title}>Onoodor her baina?</Text>
-        </View>
-        <View style={styles.vm6}>
-          <Text style={styles.txt1}> Дагасан сэдвүүд:</Text>
-        </View>
-        <View style={styles.view2}>
-          <ScrollView horizontal={true}>
-            <View style={styles.vm2}>
-              <Text style={styles.txt2}>Gantsaardal</Text>
-            </View>
-            <View style={styles.vm2}>
-              <Text style={styles.txt2}>Gantsaardal</Text>
-            </View>
-            <View style={styles.vm2}>
-              <Text style={styles.txt2}>Gantsaardal</Text>
-            </View>
-            <View style={styles.vm2}>
-              <Text style={styles.txt2}>Gantsaardal</Text>
-            </View>
-            <View style={styles.vm2}>
-              <Text style={styles.txt2}>Gantsaardal</Text>
-            </View>
-            <View style={styles.vm2}>
-              <Text style={styles.txt2}>Gantsaardal</Text>
-            </View>
-            <View style={styles.vm2}>
-              <Text style={styles.txt2}>Gantsaardal</Text>
-            </View>
-          </ScrollView>
-        </View>
-      </ScrollView>
+          <Text style={styles.title}>Өнөөдөр хэр байна?</Text>
+          </View>
+                <View style={styles.dagasan} >
+                <Text style={styles.txt1}> Дагасан сэдвүүд:</Text>
+                </View>
+                <View style={styles.view2}>
+            <ScrollView horizontal={true}>
+                <View style={styles.vm2}>
+                <Text style={styles.txt2}>Ганцаардал</Text>
+                </View>
+                <View style={styles.vm2}>
+                <Text style={styles.txt2}>Стресс</Text>
+                </View>
+                <View style={styles.vm2}>
+                <Text style={styles.txt2}>Гэр бүлийн харилцаа</Text>
+                </View>
+                <View style={styles.vm2}>
+                <Text style={styles.txt2}>Хайр дурлал</Text>
+                </View>
+                <View style={styles.vm2}>
+                <Text style={styles.txt2}>Зорилго</Text>
+                </View>
+                <View style={styles.vm2}>
+                <Text style={styles.txt2}>Сэтгэл хөдлөл</Text>
+                </View>
+                <View style={styles.vm2}>
+                <Text style={styles.txt2}>#crush</Text>
+                </View>
+                <View style={styles.vm2}>
+                <Text style={styles.txt2}>#seendelt</Text>
+                </View>
+            </ScrollView>
+                </View>
+
+
+
+
+
+    <View id="field" style={styles.field} >
+    <View style={styles.vm1_2}>
+    <Image
+    style={styles.pro1_2}
+    source={require("../../src/images/natural.jpg")}
+    />
+        <View>
+          <Text style={styles.name}>Gatsuurhan<Entypo name="emoji-sad" style={styles.emoji}  /></Text>
+          <Text id="ner" style={styles.name2}>11min</Text> 
+          
+        </View> 
+        
+    </View>     
+              
+              
+              <View style={styles.vm2_2}>
+                <Text style={styles.txt2_2}>Ганцаардал</Text>
+              </View>
+    
+                <View>
+                <Text style={styles.setgegdel}>Lorem Ipsum is simply dummy text of the printing and typesettina to make a type five centuries, but also the leap into electronic typesetting, remaining essentially uncha</Text>
+                <Text style={styles.setgegdel1}>0 сэтгэгдэл</Text>
+                </View>
+                <View style={styles.zuraas}></View>
+                <View style={styles.icon}>
+                <AntDesign name="like2" style={styles.Icon1} />
+                <FontAwesome name="comment-o" style={styles.Icon1} />
+                <AntDesign name="sharealt" style={styles.Icon1} />
+                </View>
+    </View>
+    <View id="field" style={styles.field} >
+    <View style={styles.vm1_2}>
+    <Image
+    style={styles.pro1_2}
+    source={require("../../src/images/natural.jpg")}
+    />
+        <View>
+          <Text style={styles.name}>Gatsuurhan<Entypo name="emoji-sad" style={styles.emoji}/></Text>
+          <Text id="ner" style={styles.name2}>11min</Text> 
+          
+        </View> 
+        
+    </View>     
+              
+              
+              <View style={styles.vm2_2}>
+                <Text style={styles.txt2_2}>Ганцаардал</Text>
+              </View>
+    
+                <View>
+                <Text style={styles.setgegdel}>Lorem Ipsum is simply dummy text of the printing and typesettina to make a type five centuries, but also the leap into electronic typesetting, remaining essentially uncha</Text>
+                <Text style={styles.setgegdel1}>0 сэтгэгдэл</Text>
+                </View>
+                <View style={styles.zuraas}></View>
+                <View style={styles.icon}>
+                <AntDesign name="like2" style={styles.Icon1} />
+                <FontAwesome name="comment-o" style={styles.Icon1} />
+                <AntDesign name="sharealt" style={styles.Icon1} />
+                </View>
+    </View>
+    <View id="field" style={styles.field} >
+    <View style={styles.vm1_2}>
+    <Image
+    style={styles.pro1_2}
+    source={require("../../src/images/natural.jpg")}
+    />
+        <View>
+          <Text style={styles.name}>Gatsuurhan<Entypo name="emoji-sad" style={styles.emoji}/></Text>
+          <Text id="ner" style={styles.name2}>11min</Text> 
+          
+        </View> 
+        
+    </View>     
+              
+              
+              <View style={styles.vm2_2}>
+                <Text style={styles.txt2_2}>Ганцаардал</Text>
+              </View>
+    
+                <View>
+                <Text style={styles.setgegdel}>Lorem Ipsum is simply dummy text of the printing and typesettina to make a type five centuries, but also the leap into electronic typesetting, remaining essentially uncha</Text>
+                <Text style={styles.setgegdel1}>0 сэтгэгдэл</Text>
+                </View>
+                <View style={styles.zuraas}></View>
+                <View style={styles.icon}>
+                <AntDesign name="like2" style={styles.Icon1} />
+                <FontAwesome name="comment-o" style={styles.Icon1} />
+                <AntDesign name="sharealt" style={styles.Icon1} />
+                </View>
+    </View>
+    <View id="field" style={styles.field} >
+    <View style={styles.vm1_2}>
+    <Image
+    style={styles.pro1_2}
+    source={require("../../src/images/natural.jpg")}
+    />
+        <View>
+          <Text style={styles.name}>Gatsuurhan<Entypo name="emoji-sad" style={styles.emoji}/></Text>
+          <Text id="ner" style={styles.name2}>11min</Text> 
+          
+        </View> 
+        
+    </View>     
+              
+              
+              <View style={styles.vm2_2}>
+                <Text style={styles.txt2_2}>Ганцаардал</Text>
+              </View>
+    
+                <View>
+                <Text style={styles.setgegdel}>Lorem Ipsum is simply dummy text of the printing and typesettina to make a type five centuries, but also the leap into electronic typesetting, remaining essentially uncha</Text>
+                <Text style={styles.setgegdel1}>0 сэтгэгдэл</Text>
+                </View>
+                <View style={styles.zuraas}></View>
+                <View style={styles.icon}>
+                <AntDesign name="like2" style={styles.Icon1} />
+                <FontAwesome name="comment-o" style={styles.Icon1} />
+                <AntDesign name="sharealt" style={styles.Icon1} />
+                </View>
+    </View>
+    <View id="field" style={styles.field} >
+    <View style={styles.vm1_2}>
+    <Image
+    style={styles.pro1_2}
+    source={require("../../src/images/natural.jpg")}
+    />
+        <View>
+          <Text style={styles.name}>Gatsuurhan<Entypo name="emoji-sad" style={styles.emoji}/></Text>
+          <Text id="ner" style={styles.name2}>11min</Text> 
+          
+        </View> 
+        
+    </View>     
+              
+              
+              <View style={styles.vm2_2}>
+                <Text style={styles.txt2_2}>Ганцаардал</Text>
+              </View>
+    
+                <View>
+                <Text style={styles.setgegdel}>Lorem Ipsum is simply dummy text of the printing and typesettina to make a type five centuries, but also the leap into electronic typesetting, remaining essentially uncha</Text>
+                <Text style={styles.setgegdel1}>0 сэтгэгдэл</Text>
+                </View>
+                <View style={styles.zuraas}></View>
+                <View style={styles.icon}>
+                <AntDesign name="like2" style={styles.Icon1} />
+                <FontAwesome name="comment-o" style={styles.Icon1} />
+                <AntDesign name="sharealt" style={styles.Icon1} />
+                </View>
+    </View>
+    <View id="field" style={styles.field} >
+    <View style={styles.vm1_2}>
+    <Image
+    style={styles.pro1_2}
+    source={require("../../src/images/natural.jpg")}
+    />
+        <View>
+          <Text style={styles.name}>Gatsuurhan <Entypo name="emoji-sad" style={styles.emoji}/></Text>
+          <Text id="ner" style={styles.name2}>11min</Text> 
+          
+        </View> 
+        
+    </View>     
+              
+              
+              <View style={styles.vm2_2}>
+                <Text style={styles.txt2_2}>Ганцаардал</Text>
+              </View>
+    
+                <View>
+                <Text style={styles.setgegdel}>Lorem Ipsum is simply dummy text of the printing and typesettina to make a type five centuries, but also the leap into electronic typesetting, remaining essentially uncha</Text>
+                <Text style={styles.setgegdel1}>0 сэтгэгдэл</Text>
+                </View>
+                <View style={styles.zuraas}></View>
+                <View style={styles.icon}>
+                <AntDesign name="like2" style={styles.Icon1} />
+                <FontAwesome name="comment-o" style={styles.Icon1} />
+                <AntDesign name="sharealt" style={styles.Icon1} />
+                </View>
+    </View>
+    </ScrollView>
     </SafeAreaView>
   );
 }
@@ -56,54 +250,196 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#fff",
+    backgroundColor: "#EBECF0",
   },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
+  // title: {
+  //   fontSize: 20,
+  //   // fontWeight: "bold",
+  //   marginBottom: 20,
+  //   backgroundColor: "#EBECF0",
+  // },
   vm1: {
     flexDirection: "row",
     height: 60,
     // width: 400,
     alignItems: "center",
     // justifyContent: "center",
-    borderWidth: 1,
-    backgroundColor: "red",
+    borderWidth: 0,
+    backgroundColor: "white",
   },
   pro1: {
-    borderRadius: 30,
+    borderRadius: 400,
     flex: 1,
     height: 40,
     marginLeft: 25,
   },
+
+
   title: {
     flex: 9,
-    fontSize: "20",
-    marginTop: 7,
+    fontSize: 17,
     marginLeft: 10,
+    color: "#2f2f2f",
   },
+
   txt1: {
-    color: "grey",
-    marginLeft: 20,
+    color: "#6f6f6f",  
+    marginLeft: 10,
   },
   txt2: {
     marginHorizontal: 10,
     fontSize: 13,
+    color: "#202020",
   },
   vm2: {
-    backgroundColor: "grey",
+    backgroundColor: "#EBECF0",
     borderRadius: 30,
     justifyContent: "center",
     // paddingHorizontal: -10,
-    height: 50,
+    height: 35,
     marginHorizontal: 5,
     // paddingVertical: -5,
   },
 
   view2: {
     height: 50,
-    backgroundColor: "yellow",
+    backgroundColor: "white",
+    marginTop: 0,
   },
+  // undsenvw1:{
+  //   height: 100,
+  //   borderColor
+  // },
+  // pro2:{
+  //   borderRadius: 30,
+  //   flex: 1,
+  //   height: 40,
+  //   marginLeft: 25,
+  // },
+  // name:{
+  //   flex: 8,
+  //   fontSize: "20",
+  //   marginTop: 7,
+  //   marginLeft: 10,
+  // },
+  dagasan:{
+    flexDirection: "row",
+    marginTop: 2,
+    height: 40,
+    // width: 400,
+    alignItems: "center",
+    // justifyContent: "center",
+    borderWidth: 0,
+    backgroundColor: "white",
+  },
+  field:{
+      marginTop: 5,
+      backgroundColor: "white",
+      // justifyContent: "center",
+      height: 245,
+  },
+  vm1_2: {
+    flexDirection: "row",
+    height: 180,
+    // width: 400,
+    // alignItems: "center",  
+    // justifyContent: "center",
+    borderWidth: 0,
+    backgroundColor: "white",
+  },
+  pro1_2: {
+    borderRadius: 400,
+    
+    height: 30,
+    marginLeft: 20,
+    marginTop:10,
+    width:30,
+  },
+  name: {
+    fontSize: "15",
+    marginLeft: 10,
+    fontWeight:'bold',
+    marginTop: 8,
+
+  },
+  name2:{
+    fontSize: "14",
+    marginLeft: 10,
+    
+  },
+  vm2_2:{    
+    backgroundColor: "#EBECF0",
+    borderRadius: 30,
+    alignItems: "center",
+    justifyContent:"center",
+  // paddingHorizontal: -10,
+    height: 25,
+  //  marginHorizontal: 5,
+  // paddingVertical: -5,
+    width:100,
+    marginTop:-120,
+    marginLeft:20,
+},
+txt2_2:{
+  marginHorizontal: 10,
+    fontSize: 13,
+    color: "#4f4f4f",
+},
+setgegdel:{
+  marginLeft:20,
+  marginRight:20,
+  marginTop: 12,
+},
+setgegdel1:{
+  textAlign: "right",
+  marginRight: 13,
+  color: '#grey',
+  height: 25,
+  marginTop: 7,
+  color: '#6f6f6f',
+},
+Icon1:{
+  fontSize: 20,
+  marginLeft: 8,
+  color: "#5f5f5f",
+
+  justifyContent: "center"
+},
+Icon2:{
+  fontSize: 20,
+  marginLeft: 8,
+  color: "#5f5f5f",
+  flex:1,
+},
+Icon3:{
+  fontSize: 20,
+  marginLeft: 8,
+  color: "#5f5f5f",
+  flex:1,
+
+},
+icon:{
+  flexDirection: "row",
+  justifyContent: "space-around",
+  justifyContent: "center",
+  marginTop: 14,
+  flex: 1,
+  justifyContent: "space-around",
+
+
+  
+},
+zuraas:{
+  height:1,
+  width: "90%",
+  backgroundColor: "#EBECF0",
+  justifyContent: "center",
+  marginLeft: 15,
+
+},
+emoji:{
+  color: "#2C7DA0",
+  marginLeft: 10,
+  fontSize: 15,
+}
 });
--- Blindcare/src/screens/TestScreen.js
+++ Blindcare/src/screens/TestScreen.js
@@ -1,15 +1,170 @@
-import * as React from "react";
-import { View, Text } from "react-native";
 
+import * as React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  Image,
+  StyleSheet,
+} from "react-native";
 export default function TestScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      
       <Text
         onPress={() => navigation.navigate("Home")}
         style={{ fontSize: 26, fontWeight: "bold" }}
       >
-        Details Screen
       </Text>
+
+   
+      
+      <SafeAreaView style={styles.container}>
+        <ScrollView>
+          <View style={styles.main}>
+          <ScrollView horizontal={true}>
+                <View style={styles.vm2}>
+                <Text style={styles.txt2}>Ганцаардал</Text>
+                </View>
+                <View style={styles.vm2}>
+                <Text style={styles.txt2}>Стресс</Text>
+                </View>
+                <View style={styles.vm2}>
+                <Text style={styles.txt2}>Гэр бүлийн харилцаа</Text>
+                </View>
+                <View style={styles.vm2}>
+                <Text style={styles.txt2}>Хайр дурлал</Text>
+                </View>
+                <View style={styles.vm2}>
+                <Text style={styles.txt2}>Зорилго</Text>
+                </View>
+                <View style={styles.vm2}>
+                <Text style={styles.txt2}>Сэтгэл хөдлөл</Text>
+                </View>
+                <View style={styles.vm2}>
+                <Text style={styles.txt2}>#crush</Text>
+                </View>
+                <View style={styles.vm2}>
+                <Text style={styles.txt2}>#seendelt</Text>
+                </View>
+            </ScrollView>
+          </View>
+          <View style={styles.havtan}>
+                <View>
+                <Text style={styles.cl}>Үнэгүй</Text>
+              <Text style={styles.bold}>Хэрэглэгчийн судалгаа</Text>
+                </View>
+                <Image style={styles.zurag}
+    source={require("../../src/images/natural.jpg")}
+    />
+
+          </View>
+          <View style={styles.havtan}>
+                <View>
+                <Text style={styles.cl}>Үнэгүй</Text>
+              <Text style={styles.bold}>Хэрэглэгчийн судалгаа</Text>
+                </View>
+                <Image style={styles.zurag}
+    source={require("../../src/images/natural.jpg")}
+    />
+
+          </View>
+          <View style={styles.havtan}>
+                <View>
+                <Text style={styles.cl}>Үнэгүй</Text>
+              <Text style={styles.bold}>Хэрэглэгчийн судалгаа</Text>
+                </View>
+                <Image style={styles.zurag}
+    source={require("../../src/images/natural.jpg")}
+    />
+
+          </View>
+          <View style={styles.havtan}>
+                <View>
+                <Text style={styles.cl}>Үнэгүй</Text>
+              <Text style={styles.bold}>Хэрэглэгчийн судалгаа</Text>
+                </View>
+                <Image style={styles.zurag}
+    source={require("../../src/images/natural.jpg")}
+    />
+
+          </View>
+          <View style={styles.havtan}>
+                <View>
+                <Text style={styles.cl}>Үнэгүй</Text>
+              <Text style={styles.bold}>Хэрэглэгчийн судалгаа</Text>
+                </View>
+                <Image style={styles.zurag}
+    source={require("../../src/images/natural.jpg")}
+    />
+
+          </View>
+          
+        </ScrollView>
+    </SafeAreaView>
     </View>
   );
 }
+const styles = StyleSheet.create({
+
+
+  vm2: {
+    marginBottom:10,
+    // backgroundColor: "#EBECF0",
+    borderRadius: 20,
+    borderColor: "grey",
+    borderWidth: 1,
+    justifyContent: "center",
+    // paddingHorizontal: -10,
+    height: 35,
+    marginHorizontal: 5,
+    // paddingVertical: -5,
+  },
+  txt2: {
+    marginHorizontal: 10,
+    fontSize: 13,
+    color: "#202020",
+  },
+  havtan:{
+    height: 140,
+    backgroundColor: "white",
+    borderRadius: 15,
+    width: "95%",
+    // marginLeft: 10,
+    marginTop: 10,
+    marginLeft: 10,
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    
+  },
+  zurag:{
+    width: 80,
+    height: 100,
+    borderRadius: 10,
+  },
+  container:{
+    // backgroundColor: "#E5E5E5"
+  },
+  bold:{
+    fontWeight: "bold",
+  },
+  cl:{
+    color: "#ff6700",
+    fontWeight: "bold",
+  }
+ 
+
+
+
+
+
+
+
+
+
+
+});
+
